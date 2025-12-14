4fffb6a5db8d9e5924a40f9614dc1c053f952e4a
Ersin Tarhan <ersintarhan@gmail.com>
2025-12-14 05:19:20 +0300

    Add ssh keys

--- home/ersin/programs.nix
+++ home/ersin/programs.nix
@@ -195,6 +195,12 @@
     nix-direnv.enable = true; # Faster nix integration
   };
 
+  # === SSH Public Keys ===
+  home.file.".ssh/id_ed25519.pub".text =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGXlNY23o4h8SeXTVhK8reQSSbblhmka3MjOZrwtTyvx";
+  home.file.".ssh/id_rsa.pub".text =
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCvcwwoUSFyFi56PUlbGwY57JnzBSKpVgTKkZcT8LQnwLIy6oNObVU/vBcJFG9haVZI/KimKRopPoPltohc54fq0mZOtAfZseF4CbPHXAXIoZokceUkPbYEBxfEeGhmdaoq1LsbweSgdEfiweQScMQkkPNPbYZ3eL2DetI51vrAla5aBSr3yDOhVYUU9GjwVhKHIR0XQmuhWqiUVH08gpDiLzfS3MK62R3TzzuKyQ0IwLCBAuJqHthwpixqtq0hS506PrCH1WlhkzjYFL4HQM4etkqBzgkXyVubIZy0KQKfDBmWWeCe1yz81KBGSG8HT85hQO6yQpTUwBkCNFAqvBUy8/BPjY7hzNQhkRYFWIz/ztGAclJ1+q+kIBTPoXjXG6aYMiTGKkIIEQyQLYuKdBb+Y9THf4LWxOwqI/21RWNyLMxh4ERuUN3NnOhDe/DX9YZQ/5rxRT4vorBIlxDz/jjWYmadmOF/IFF+SNcjH5JqJYdJ4YXlhhy6HX0rvwMhLp0=";
+
   # === SSH ===
   programs.ssh = {
     enable = true;
