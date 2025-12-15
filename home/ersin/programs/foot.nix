9c1bd2ca1036b281f5b859fbe662f09ee00d5880
Ersin Tarhan <ersintarhan@gmail.com>
2025-12-15 07:27:11 +0300

    feat(foot): Enable copy-on-select functionality
    Enabled the "copy-on-select" behavior in the Foot terminal configuration. When text is selected with the mouse, it will now be automatically copied to the system clipboard, aligning with the user's preference.

--- home/ersin/programs/foot.nix
+++ home/ersin/programs/foot.nix
@@ -4,16 +4,17 @@
 {
   programs.foot = {
     enable = true;
-    
+
     # Enable server mode for instant new terminals
     server.enable = true;
 
     settings = {
       # === Main settings ===
       main = {
-        font = "JetBrainsMono Nerd Font:size=12";
+        font = "JetBrainsMono Nerd Font:size=14";
         term = "xterm-256color";
         pad = "10x10"; # More padding for a cleaner look
+        selection-target = "clipboard"; # Copy on select
       };
 
       # === Scrollback ===
@@ -43,26 +44,26 @@
       colors = {
         foreground = "cdd6f4"; # Text
         background = "1e1e2e"; # Base
-        
+
         # Regular Colors
-        regular0 = "45475a";  # Surface1
-        regular1 = "f38ba8";  # Red
-        regular2 = "a6e3a1";  # Green
-        regular3 = "f9e2af";  # Yellow
-        regular4 = "89b4fa";  # Blue
-        regular5 = "f5c2e7";  # Pink
-        regular6 = "94e2d5";  # Teal
-        regular7 = "bac2de";  # Subtext1
+        regular0 = "45475a"; # Surface1
+        regular1 = "f38ba8"; # Red
+        regular2 = "a6e3a1"; # Green
+        regular3 = "f9e2af"; # Yellow
+        regular4 = "89b4fa"; # Blue
+        regular5 = "f5c2e7"; # Pink
+        regular6 = "94e2d5"; # Teal
+        regular7 = "bac2de"; # Subtext1
 
         # Bright Colors
-        bright0 = "585b70";   # Surface2
-        bright1 = "f38ba8";  # Red
-        bright2 = "a6e3a1";  # Green
-        bright3 = "f9e2af";  # Yellow
-        bright4 = "89b4fa";  # Blue
-        bright5 = "f5c2e7";  # Pink
-        bright6 = "94e2d5";  # Teal
-        bright7 = "a6adc8";  # Subtext0
+        bright0 = "585b70"; # Surface2
+        bright1 = "f38ba8"; # Red
+        bright2 = "a6e3a1"; # Green
+        bright3 = "f9e2af"; # Yellow
+        bright4 = "89b4fa"; # Blue
+        bright5 = "f5c2e7"; # Pink
+        bright6 = "94e2d5"; # Teal
+        bright7 = "a6adc8"; # Subtext0
       };
 
       # === Key Bindings ===
@@ -80,10 +81,10 @@
         font-reset = "Control+0";
         url-select = "Control+Shift+u";
       };
-      
+
       # === Other settings ===
       # Uncomment if you want foot to have a title bar in tiling mode
-      # csd.preferred = "server"; 
+      # csd.preferred = "server";
     };
   };
 }
