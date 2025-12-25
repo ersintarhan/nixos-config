cdd3243fc35f9beabe0978787dbfd1b4414fa84e
Ersin Tarhan <ersintarhan@gmail.com>
2025-12-25 06:37:08 +0300

    refactor: remove distrobox container definitions from Home Manager
    - Remove debian and arch containers from distrobox config
    - Move container management outside Home Manager for better flexibility
    - Containers can now be created and managed manually via CLI
    - Allows easier customization and faster updates

--- home/ersin/containers/distrobox.nix
+++ home/ersin/containers/distrobox.nix
@@ -12,24 +12,5 @@
       skip_workdir = "0";
     };
 
-    containers = {
-      # Debian Trixie - isolated dev environment
-      # For postgresql, redis, testing etc.
-      debian = {
-        image = "debian:trixie";
-        entry = true;
-        init = true; # systemd enabled
-        home = "~/.distrobox/home/debian"; # isolated home
-        additional_packages = "git curl wget build-essential micro lnav";
-      };
-
-      # Arch Linux - shared home, quick access
-      # paru: git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si
-      arch = {
-        image = "archlinux:latest";
-        entry = true;
-        additional_packages = "base-devel git fish";
-      };
-    };
   };
 }
